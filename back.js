e55b677f449d47c0f72cc8c2e058e981b27f57cf
Tro-fish <dreamnawas@naver.com>
2021-10-23 13:26:16 +0900

    update html_1

---
+++ back.js
@@ -0,0 +1,58 @@
+           let saved_account =[1111,2222];
+           let new_account=0;
+           let num=1;
+           
+           function enter1()
+           {
+               console.log(new_account);
+           }
+           function button_0(){              
+               new_account+= new_account + 0*num;
+               num*=10;
+               console.log(new_account);
+           }
+           function button_1(){                                  
+               new_account+= new_account + 1*num;
+               num*=10;
+               console.log(new_account);
+           }
+           function button_2(){           
+               new_account+= new_account + 2*num;
+               num*=10;
+               console.log(new_account);
+           }
+           function button_3(){          
+               new_account+= new_account + 3*num;
+               num*=10;
+               console.log(new_account);
+           }
+           function button_4(){           
+               new_account+= new_account + 4*num;
+               num*=10;
+               console.log(new_account);
+           }
+           function button_5(){        
+               new_account+= new_account + 5*num;
+               num*=10;
+               console.log(new_account);
+           }
+           function button_6(){           
+               new_account+= new_account + 6*num;
+               num*=10;
+               console.log(new_account);
+           }
+           function button_7(){            
+               new_account+= new_account + 7*num;
+               num*=10;
+               console.log(new_account);
+           }
+           function button_8(){           
+               new_account+= new_account + 8*num;
+               num*=10;
+               console.log(new_account);
+           }
+           function button_9(){                                 
+               new_account+= new_account + 9*num;
+               num*=10;
+               console.log(new_account);
+           }
